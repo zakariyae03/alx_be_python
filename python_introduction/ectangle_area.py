bd15e922e5e104f851053543157198f62e092651
zakariyae03 <zakariyae.moustakim.03@gmail.com>
2025-01-10 02:35:31 +0100

    Create ectangle_area.py

---
+++ python_introduction/ectangle_area.py
@@ -0,0 +1,9 @@
+# Define variables
+length = 10  # Length of the rectangle
+width = 5    # Width of the rectangle
+
+# Calculate the area of the rectangle
+area = length * width
+
+# Print the calculated area
+print(f"The area of the rectangle is: {area}")
