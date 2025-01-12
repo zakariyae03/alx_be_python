9522a028c547a5e200a52ea0f6c2ea344c900fbf
zakariyae03 <zakariyae.moustakim.03@gmail.com>
2025-01-12 23:44:14 +0100

    Update shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -2,7 +2,7 @@ def display_menu():
     """
     Displays the menu options for the shopping list manager.
     """
-    print("\nShopping List Manager")
+    print("Shopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
