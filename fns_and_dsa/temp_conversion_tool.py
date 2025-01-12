760ee2f64a8c51472ddf6732be8cf561629a4f8a
zakariyae03 <zakariyae.moustakim.03@gmail.com>
2025-01-12 23:38:43 +0100

    Update temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,6 +1,6 @@
 # Global conversion factors
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
     """
@@ -41,3 +41,4 @@ def main():
         print(e)
 
 if __name__ == "__main__":
+    main()
